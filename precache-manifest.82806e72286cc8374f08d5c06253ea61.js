a8b7942300d15a6f4a04fc33464fa0f1b5daced1
kamal das <kamalphysics496@gmail.com>
2020-06-22 13:07:31 +0530

    Updates

---
+++ precache-manifest.82806e72286cc8374f08d5c06253ea61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a368287570c176267a15729d536343db",
+    "url": "/onlineclass/index.html"
+  },
+  {
+    "revision": "159b4c3dfc6159f79769",
+    "url": "/onlineclass/static/js/2.f8570e23.chunk.js"
+  },
+  {
+    "revision": "de5933bbfe8f970953aa351dcd16a9a6",
+    "url": "/onlineclass/static/js/2.f8570e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59073eb44ce1a1c54d5",
+    "url": "/onlineclass/static/js/main.623c0c6d.chunk.js"
+  },
+  {
+    "revision": "6bd77a2556af250388e6",
+    "url": "/onlineclass/static/js/runtime-main.3395b05f.js"
+  }
+]);
